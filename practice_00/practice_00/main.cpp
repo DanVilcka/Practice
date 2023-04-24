73e91b62635d0a2c527a495e67b8ea5d2c518ffa
Daniil <98778382+DanVilcka@users.noreply.github.com>
2023-04-24 18:45:35 +0300

    Practice_00

---
+++ practice_00/practice_00/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  practice_00
+//
+//  Created by Daniil Vilchinskiy on 24.04.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
